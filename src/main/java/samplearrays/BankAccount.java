c7e37cba2b5ebdc1c59cc6ef8985d350b7e76b37
Adam-Ibourg <adamibourg4@gmail.com>
2026-09-26 12:39:28 +0000

    Implement deposit method in BankAccount

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -4,14 +4,28 @@ public class BankAccount {
 
     String name;
     double currentBalance;
-    //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    double[] transactions = new double[1000];
+    // creating a variable to keep track of the transactions array since java doesn't have a method to append elements to arrays
+    int transactionIndex = 0;
 
     public BankAccount(String name, int startingBalance){
-
+        this.name = name;
+        this.currentBalance = startingBalance;
     }
 
     public void deposit(double amount){
+        if (amount <= 0) {
+            // error message for unsuccessful deposits
+            System.out.println("Enter a positive amount for deposit");
+            return;
+        }
 
+        this.currentBalance += amount;  // adding the deposit amount
+        transactions[transactionIndex] = amount;  // recording the transaction in the array
+        transactionIndex++;
+
+        // displaying a successful deposit message
+        System.out.println("Successful deposit: {amount = " + amount + ", name: " + name + ", new balance = " + currentBalance + '}');
     }
 
     public void withdraw(double amount){
